595823e03889d61af3ef79755f34ad0b825f76c3
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2021-05-31 19:51:49 -0300

    First feature completed

--- App.js
+++ App.js
@@ -1,18 +1,6 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Home } from './src/pages/Home';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>React Native</Text>
-    </View>
-  );
+export default function App(){
+  return <Home />
 }
-
-const styles = StyleSheet.create({
-  container : {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-});
---
+++ src/pages/Home.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react';
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  TextInput, 
+  Platform, 
+  TouchableOpacity,
+  FlatList,
+  StatusBar 
+} from 'react-native';
+
+export function Home() {
+  const [newSkill, setNewSkill ] = useState('');
+  const [ mySkills , setMySkills ] = useState([]);
+
+  function handleAddNewSkill() {
+    setMySkills(oldState => [...oldState, newSkill]);
+  }
+
+  return (
+    <View style={styles.container}>
+      <StatusBar 
+        backgroundColor='#121015' 
+      />
+      <Text style={styles.title}>Welcome, Ricardo</Text>
+
+      <TextInput 
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
+        onChangeText={setNewSkill}
+      />
+      <TouchableOpacity 
+        style={styles.button}
+        activeOpacity={.7}
+        onPress={handleAddNewSkill}
+      >
+        <Text 
+          style={styles.buttonText}
+        >
+          Add
+        </Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, { marginVertical: 50 }]}>
+        My Skills
+      </Text>
+      
+      {
+        mySkills.map(skill => (
+          <TouchableOpacity key={skill} style={styles.buttonSkill}>
+            <Text style={styles.textSkill}>
+                {skill}
+            </Text>
+          </TouchableOpacity> 
+        ))
+      }
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,    
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#FFF',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20
+  },
+  buttonText : {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold'
+  },
+  buttonSkill: {    
+    backgroundColor: '#1F1E25',
+    padding: 15,    
+    borderRadius: 50,
+    alignItems: 'center', 
+    marginVertical: 10,   
+  },
+  textSkill: {
+    color: '#fff',    
+    fontSize: 22,
+    fontWeight: 'bold',    
+  }
+
+});
